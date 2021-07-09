ef8e69d0736b5576e8adf23fff921a8c7aae5a42
Anton <a.sharudylo@gmail.com>
2021-07-09 23:14:27 +0300

    modal_copy

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './sass/main.scss';
 import cardsEvents from './tpl/card_events.hbs';
+// import createModalMarkup from './tpl/card-modal.hbs';
 import './js/modal.js';
 import SearchService from './js/api/fetchListApi';
 
