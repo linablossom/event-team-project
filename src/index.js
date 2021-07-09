8f9c465b8b683de3eefb5cc2ef795fe150b30024
Taras Dovbyus <79213421+dstaras@users.noreply.github.com>
2021-07-09 12:28:17 +0300

    fetch list commit

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 import './sass/main.scss';
 import cardsEvents from './tpl/card_events.hbs';
+
+import SearchService from './js/api/fetchListApi';
+
+// const searchService = new SearchService();
+// searchService.fetchApiEvent();
