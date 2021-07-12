a849963b8f30481cc60dc83d95b6faa16b1b13f7
Anton <a.sharudylo@gmail.com>
2021-07-12 12:00:02 +0300

    add time and image url

--- src/js/renderers/renderItem.js
+++ src/js/renderers/renderItem.js
@@ -21,8 +21,33 @@ const renderItem = ({ id, data, loading, error, onRelated }) => {
     markupModal.innerHTML = 'loading...';
     return;
   }
+
+  //шукаю в масиві картинку з розмірами 640 х 427
+
+  function renderImg(data) {
+    const images = data.images;
+    const imageForModal = images.find(image => image.width === 640 && image.height === 427);
+    return imageForModal.url;
+  }
+
+  //обрізаю секунди
+
+  function removeSecond(data) {
+    const time = data.dates.start.localTime;
+    const timeWithoutSec = time.slice(0, -3);
+    return timeWithoutSec;
+  }
+
+  //підготовлюю дані для передачі в hbs
+
+  const preparedDataforHbs = {
+    ...data,
+    imageUrl: renderImg(data),
+    localTime: removeSecond(data),
+  };
+
   markupModal.innerHTML = '';
-  markupModal.insertAdjacentHTML('beforeend', createModalMarkup(data));
+  markupModal.insertAdjacentHTML('beforeend', createModalMarkup(preparedDataforHbs));
   markupModal.querySelector('.modal__btn-author').addEventListener('click', () => {
     onRelated(data.name);
   });
