a87eb2388edb352e633433ebd51d475b392fcaec
Alina Akompi <alina.akompi@gmail.com>
2021-07-13 02:55:41 +0300

    add pagination function

--- src/js/renderers/renderPagination.js
+++ src/js/renderers/renderPagination.js
@@ -1,3 +1,6 @@
-const renderPagination = ({ page, maxPage, onChange }) => {};
+import { pagination } from '../utils';
+const renderPagination = ({ page, maxPage, onChange }) => {
+  const pages = pagination(page, maxPage);
+};
 
 export default renderPagination;
